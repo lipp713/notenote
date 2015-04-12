dee175c06f24afb9b43f926f6801f1a952bae2c1
lipp713 <lipp713@gmail.com>
2015-04-12 10:56:39 +0100

    Generate Notes Controller and add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Notenote::Application.routes.draw do
   get "welcome/index"
 
   root 'welcome#index'
+
+  resources :notes
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
